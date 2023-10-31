cd59c18e4ab96010777ba1f2db4741872db0dfe5
Jouko Mälkönen <jouko.malkonen@helsinki.fi>
2023-11-01 01:11:00 +0200

    tests working again

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -11,7 +11,7 @@ class TestVarasto(unittest.TestCase):
         self.assertAlmostEqual(self.varasto.saldo, 0)
 
     def test_uudella_varastolla_oikea_tilavuus(self):
-        self.assertAlmostEqual(self.varasto.tilavuus, 1)
+        self.assertAlmostEqual(self.varasto.tilavuus, 10)
 
     def test_uudella_varastolla_negatiivinen_tilavuus(self):
         self.varasto = Varasto(-1)
